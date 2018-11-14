7a0fe7148447379b25574059164ef4641338b661
David Falusi <falusi.david94@gmail.com>
2018-11-14 21:08:12 +0100

    Add scan method

--- checkout.rb
+++ checkout.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Checkout
+  def initialize
+    @items = []
+  end
+
+  def scan(item)
+    @items.push item
+  end
 end
--- test_checkout.rb
+++ test_checkout.rb
@@ -4,7 +4,13 @@ require 'test/unit'
 require './checkout'
 
 class TestCheckout < Test::Unit::TestCase
-  def test_checkout_can_be_created
-    Checkout.new
+  def setup
+    @checkout = Checkout.new
+  end
+
+  def test_scan_item
+    item = { product_code: 'gr1', name: 'Green tea', price: '3.11' }
+    items = @checkout.scan(item)
+    assert_equal items.count, 1
   end
 end
