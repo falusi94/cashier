169d5afea5d8f4da8820dbb2cd806a4caaff7ca6
David Falusi <falusi.david94@gmail.com>
2018-11-14 22:01:30 +0100

    Add sumup for total method

--- test_checkout.rb
+++ test_checkout.rb
@@ -6,15 +6,24 @@ require './checkout'
 class TestCheckout < Test::Unit::TestCase
   def setup
     @checkout = Checkout.new
+    @green_tea = { product_code: 'gr1', name: 'Green tea', price: 3.11 }
+    @strawberries = { product_code: 'sr1', name: 'Strawberries', price: 5 }
+    @coffee = { product_code: 'cf1', name: 'Coffee', price: 11.23 }
   end
 
   def test_scan_item
-    item = { product_code: 'gr1', name: 'Green tea', price: '3.11' }
-    items = @checkout.scan(item)
+    items = @checkout.scan(@green_tea)
     assert_equal items.count, 1
   end
 
   def test_total_count_for_empty_cart
     assert_equal @checkout.total, 0
+  end
+
+  def test_total_count
+    @checkout.scan(@green_tea)
+    assert_equal @checkout.total, @green_tea[:price]
+    @checkout.scan(@strawberries)
+    assert_equal @checkout.total, @green_tea[:price] + @strawberries[:price]
   end
 end
