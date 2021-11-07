25b98c81c2c4e5814ee0661ca582099ece7fd0d4
David Falusi <falusi.david94@gmail.com>
2021-11-07 16:52:42 +0100

    Init rspec

--- boot.rb
+++ boot.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require 'zeitwerk'
+require 'bundler/setup'
+groups_to_require = [:default]
+groups_to_require << :test if ENV['TEST']
+Bundler.require(*groups_to_require)
 
 loader = Zeitwerk::Loader.new
 loader.push_dir("#{__dir__}/lib")
